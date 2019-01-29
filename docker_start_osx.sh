203bb0b53b51ab1d1b58b157ba0e7b36e4790222
Adam Harvey <adam@ahprojects.com>
2019-01-29 16:36:07 +0100

    add image

--- docker_start_osx.sh
+++ docker_start_osx.sh
@@ -38,7 +38,13 @@ Visual Forensics and Advanced Metadata Extraction
 2) run 'python cli_workshop.py'
 3) download models 'python cli_workshop.py download_models'
 4) grab a few images
-5) run 'python cli_workshop.py metadata -i data/input/sheep.jpg -o output.csv'
+5) create hash 'python cli_workshop.py metadata -i data/input/sheep.jpg -o data/output/output.csv --hash'
+7) check it: 'cat data/output/output.csv'
+8) extract exif 'python cli_workshop.py metadata -i data/input/sheep.jpg -o data/output/output.csv --exif'
+9) extract faces 'python cli_workshop.py metadata -i data/input/obama.jpg -o data/output/output.csv --faces'
+10) find objects 'python cli_workshop.py metadata -i data/input/sheep.jpg -o data/output/output.csv --objects'
+11) you can also chain these commands. Try adding --hash --exif --faces --objects
+11) check results: 'cat data/output/output.csv'
  "
 # Start the docker container with access to USB devices
 # Make ports accessible to Jupyter
@@ -49,5 +55,4 @@ docker run -it --rm --privileged \
   --hostname VFRAME \
 	--volume "data_store_workshop:/data_store" \
 	--volume "$CUR_DIR:/vframe_workshop" \
-  -p $docker_port \
 	-e "USER_HTTP=1" $image "$@"
